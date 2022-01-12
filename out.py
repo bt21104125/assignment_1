788fda2b08937f272cf6aae01bf426f4e37cc85e
bt21104125 <97115865+bt21104125@users.noreply.github.com>
2022-01-13 00:00:27 +0530

    Add files via upload

---
+++ out.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\KESHAV SINGH\AppData\Local\Programs\Python\Python310\FILE 1 CODE.py
+PYHTON PROGRAM TO FIND AVERAGE OF THREE GIVEN NUMBERS
+Enter the FIRST number:- 20
+Enter the SECOND number:- 30
+Enter the THIRD number:- 40
+The Average of three given numbers is:  30.0
+***************************************************
+PYTHON PROGRAM TO CALCULATE THE INCOME TAX OF PERSON AFTER SEVERAL DEDUCTION OF MONEY (GROSS INCOME>$30000)
+Please Enter the Gross Income:$ 40000
+Please Enter the number of dependents: 3
+The total income tax of a person in $:  4200.0
+***************************************************
+PYTHON PROGRAM TO STORE VALUES IN LISTS
+Enter the SID of the student= 21104125
+Enter the NAME of the student= Keshav Singh
+Enter the GENDER M(MALE),F(FEMALE),U(UNKNOWN) use capital letters = M
+Enter the COURSE NAME of the student= Introducting To Computing 
+Enter the CGPA of the student= 9.5
+[21104125, 'Keshav Singh', 'M', 'Introducting To Computing ', 9.5]
+***************************************************
+ 
+PYTHON PROGRAM TO ENTER THE MARKS OF FIVE STUDENTS
+Enter the Marks of First Student:23
+Enter the Marks of Second Student: 22
+Enter the Marks of Third Student: 25
+Enter the Marks of Fourth Student: 26
+Enter the Marks of Fifth Student: 24
+The Lists of marks of five students are as follows:  [23, 22, 25, 26, 24]
+ 
+***************************************************
+PYTHON PROGRAM TO PRINT A SPECIFIED LIST IN A MANNER
+THIS IS THE FIRST PART OF THE 5TH QUESTION
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+THIS IS THE SECOND PART OF THE 5TH QUESTION
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
